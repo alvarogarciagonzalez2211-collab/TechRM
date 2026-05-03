106feaa5eff2e53cfa5eff0f4679b7ebef8594fd
unknown <alvarogarciagonzalez2211@gmail.com>
2026-05-03 14:43:35 +0200

    Primera actualización: contenido y posible integración de modo claro

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,30 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   res.render('index', {
-    title: 'TechRM - Hardware & Software'
+    title: 'TechRM - Hardware & Software',
+    currentPage: 'index'
   });
 });
 
 router.get('/hardware', (req, res) => {
   res.render('hardware', {
     title: 'TechRM - Hardware',
-    success: req.query.success === 'true'
+    success: req.query.success === 'true',
+    currentPage: 'hardware'
+  });
+});
+
+router.get('/software', (req, res) => {
+  res.render('software', {
+    title: 'TechRM - Software',
+    currentPage: 'software'
+  });
+});
+
+router.get('/nosotros', (req, res) => {
+  res.render('nosotros', {
+    title: 'TechRM - Sobre Nosotros',
+    currentPage: 'nosotros'
   });
 });
 

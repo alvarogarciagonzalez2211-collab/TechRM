bdafc643dfa6acd1d4fb8f6dd1ef768913f65319
Alvaro <alvarogarciagonzalez2211@gamil.com>
2026-05-04 13:21:54 +0200

    adaptacion fisica para metodos de hardware y correccion de colores

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,6 @@ router.get('/', (req, res) => {
 router.get('/hardware', (req, res) => {
   res.render('hardware', {
     title: 'TechRM - Hardware',
-    success: req.query.success === 'true',
     currentPage: 'hardware'
   });
 });
@@ -19,7 +18,6 @@ router.get('/hardware', (req, res) => {
 router.get('/software', (req, res) => {
   res.render('software', {
     title: 'TechRM - Software',
-    success: req.query.success === 'true',
     currentPage: 'software'
   });
 });
@@ -29,6 +27,14 @@ router.get('/montaje', (req, res) => {
     title: 'TechRM - Montaje',
     success: req.query.success === 'true',
     currentPage: 'montaje'
+  });
+});
+
+router.get('/asesoria-hardware', (req, res) => {
+  res.render('asesoria-hardware', {
+    title: 'TechRM - Asesoría Hardware',
+    success: req.query.success === 'true',
+    currentPage: 'hardware'
   });
 });
 
@@ -47,18 +53,6 @@ router.get('/nosotros', (req, res) => {
   });
 });
 
-router.post('/solicitar-servicio', (req, res) => {
-  // Aquí procesarías los datos del formulario
-  console.log('Nueva solicitud de servicio:', req.body);
-  // Por ahora, redirigir de vuelta con un mensaje de éxito
-  res.redirect('/hardware?success=true');
-});
-
-router.post('/solicitar-software', (req, res) => {
-  console.log('Nueva solicitud de software:', req.body);
-  res.redirect('/software?success=true');
-});
-
 router.post('/solicitar-montaje', (req, res) => {
   console.log('Nueva solicitud de montaje:', req.body);
   res.redirect('/montaje?success=true');
@@ -67,6 +61,11 @@ router.post('/solicitar-montaje', (req, res) => {
 router.post('/solicitar-reparacion', (req, res) => {
   console.log('Nueva solicitud de reparacion:', req.body);
   res.redirect('/reparacion?success=true');
+});
+
+router.post('/solicitar-asesoria-hardware', (req, res) => {
+  console.log('Nueva solicitud de asesoria hardware:', req.body);
+  res.redirect('/asesoria-hardware?success=true');
 });
 
 module.exports = router;
